dae308dc0a0e92699f90fa87a12ae95113255b83
Elif Sena Doğrugüven <76537265+elifdogruguven@users.noreply.github.com>
2020-12-24 13:07:23 +0300

    Add files via upload

---
+++ Eklenen Pizza Malzemelerine Göre Faturalandırma (1).py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Welcome to Elif Pizza Deliveries!")
+size = input("Which size pizza would you want? (S, M, or L) ")
+sauce_preference = input("Would you want hot sauce or tomato sauce? (H or T) ")
+add_pepperoni = input("Would you want pepperoni? (Y or N) ")
+extra_cheese = input("Would you want extra cheese? (Y or N) ")
+
+bill = 0
+
+if size == "S":
+    bill = 20
+    if sauce_preference == "H":
+        bill += 1
+    
+    if add_pepperoni == "Y":
+        bill += 1
+    
+    if extra_cheese == "Y":
+        bill += 2
+elif size == "M":
+    bill = 25
+    if sauce_preference == "H":
+        bill += 1
+        
+    if add_pepperoni == "Y":
+        bill += 1
+    
+    if extra_cheese == "Y":
+        bill += 2
+elif size == "L":
+    bill = 30
+    if sauce_preference == "H":
+        bill += 1
+        
+    if add_pepperoni == "Y":
+        bill += 2
+    
+    if extra_cheese == "Y":
+        bill += 3
+
+print("Your final bill is:",bill,"$")
+
+
+# In[ ]:
+
+
+
+
